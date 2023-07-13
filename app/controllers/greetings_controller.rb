84a518bad5dce40a9e8d1673c3ad6ca34b038808
George M'sapenda <georgecoopmsapenda@gmail.com>
2023-07-13 15:04:33 +0200

    Remove unused actions

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,42 +1,9 @@
 class GreetingsController < ApplicationController
   before_action :set_greeting, only: %i[show update destroy]
-
-  # GET /greetings
-  def index
-    @greetings = Greeting.all
-
-    render json: @greetings
-  end
-
   # GET /greetings/1
   def show
     @message = Greeting.random.first
     render json: @message
-  end
-
-  # POST /greetings
-  def create
-    @greeting = Greeting.new(greeting_params)
-
-    if @greeting.save
-      render json: @greeting, status: :created, location: @greeting
-    else
-      render json: @greeting.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /greetings/1
-  def update
-    if @greeting.update(greeting_params)
-      render json: @greeting
-    else
-      render json: @greeting.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /greetings/1
-  def destroy
-    @greeting.destroy
   end
 
   private
